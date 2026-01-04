e57f9346ef21297f6df675bb6e3a276f1e5a9dc0
TheMaura <maurosawilala@gmail.com>
2026-01-04 20:42:44 +0100

    Remover definicoes manuais de rotas para evitar conflitos com deteccao automatica

--- app/admin/_layout.tsx
+++ app/admin/_layout.tsx
@@ -11,18 +11,9 @@ export default function AdminLayout() {
         headerShown: false,
       }}
     >
-      <Stack.Screen name="index" options={{ title: 'Admin - Lendas' }} />
-      <Stack.Screen name="create" options={{ title: 'Nova Lenda' }} />
+      {/* Rotas são detectadas automaticamente pelo Expo Router baseado nos arquivos */}
+      {/* Definimos apenas rotas dinâmicas ou com configurações especiais */}
       <Stack.Screen name="edit/[id]" options={{ title: 'Editar Lenda' }} />
-      <Stack.Screen name="stories" options={{ title: 'Gestão de Histórias' }} />
-      <Stack.Screen name="stories-create" options={{ title: 'Nova História' }} />
-      <Stack.Screen name="media" options={{ title: 'Gestão de Mídias' }} />
-      <Stack.Screen name="media-create" options={{ title: 'Nova Mídia' }} />
-      <Stack.Screen name="users" options={{ title: 'Gestão de Usuários' }} />
-      <Stack.Screen name="notifications" options={{ title: 'Enviar Notificação' }} />
-      <Stack.Screen name="trophies" options={{ title: 'Gestão de Troféus' }} />
-      <Stack.Screen name="youtube-videos" options={{ title: 'Gestão de Vídeos YouTube' }} />
-      <Stack.Screen name="career-milestones" options={{ title: 'Gestão de Marcos da Carreira' }} />
     </Stack>
   );
 }
