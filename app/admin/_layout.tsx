9919f813123c7d30766c31f67a14eac42652ed0e
TheMaura <maurosawilala@gmail.com>
2026-01-04 20:47:25 +0100

    Remover definicao manual de rota para evitar conflitos com deteccao automatica

--- app/admin/_layout.tsx
+++ app/admin/_layout.tsx
@@ -11,9 +11,8 @@ export default function AdminLayout() {
         headerShown: false,
       }}
     >
-      {/* Rotas são detectadas automaticamente pelo Expo Router baseado nos arquivos */}
-      {/* Definimos apenas rotas dinâmicas ou com configurações especiais */}
-      <Stack.Screen name="edit/[id]" options={{ title: 'Editar Lenda' }} />
+      {/* Todas as rotas são detectadas automaticamente pelo Expo Router */}
+      {/* Não precisamos definir manualmente para evitar conflitos */}
     </Stack>
   );
 }
